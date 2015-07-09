2f62eb672f2c71431e1a6403b2804d27e112dafe
rimi <rimi@laughing.local>
2015-07-09 19:13:53 +0800

    Initial Commit

---
+++ MaskView-CAGradientLayer/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  MaskView-CAGradientLayer
+//
+//  Created by rimi on 15/7/9.
+//  Copyright (c) 2015年 LeeSefung. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
---
+++ MaskView-CAGradientLayer/ViewController.m
@@ -0,0 +1,27 @@
+//
+//  ViewController.m
+//  MaskView-CAGradientLayer
+//
+//  Created by rimi on 15/7/9.
+//  Copyright (c) 2015年 LeeSefung. All rights reserved.
+//
+
+#import "ViewController.h"
+
+@interface ViewController ()
+
+@end
+
+@implementation ViewController
+
+- (void)viewDidLoad {
+    [super viewDidLoad];
+    // Do any additional setup after loading the view, typically from a nib.
+}
+
+- (void)didReceiveMemoryWarning {
+    [super didReceiveMemoryWarning];
+    // Dispose of any resources that can be recreated.
+}
+
+@end
